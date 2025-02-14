7055bd85fbf993b1e48cb6ae354f69b327a68655
ahmed2402 <ahmedraza312682@gmail.com>
2025-02-15 02:10:20 +0500

    CHP 6

---
+++ Functions & Recursion/main.py
@@ -0,0 +1,40 @@
+# WAF to print the length of a list. ( list is the parameter)
+
+# def calcLen(a= []):
+#     count = 0
+#     for i in a :
+#         count += 1
+
+#     print(count)      
+          
+
+
+# calcLen([1,2,3,4,5,4,2,5,7,9,0])
+
+# WAF to print the elements of a list in a single line. ( list is the parameter)
+
+def printEl(a = []):
+    for val in a :
+        print(val, end=' ')
+
+printEl([1,3,5,7,5,34,521,3])     
+
+# WAF to find the factorial of n. (n is the parameter)
+
+# def calFact(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact = fact * i
+#     print(fact)    
+
+
+# num = int(input("enter a number : "))
+# calFact(num)
+
+# WAF to convert USD to PKR.
+
+# def currConv(usd) :
+#     pkr = usd * 300
+#     print(f"{usd} USD = {pkr} PKR")
+
+# currConv(10)    
